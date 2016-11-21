ce4580d2c8ec4b1184dd3ff10c0d11ecdf5344c5
suhanyujie <suhanyujie@qq.com>
2016-11-21 15:54:30 +0800

    开发用户注册中。。。

--- server/controllers/user_controllers.js
+++ server/controllers/user_controllers.js
@@ -1,8 +1,23 @@
 import userHandlers from '../handlers/user_handlers.js';
 
 export default function (app) {
+  // 用户注册
+  app.get('/api/user/register', (req, res) => {
+
+
+
+    var result =  {
+      flag: 1,
+      msg: 'success~'
+    };
+    res.send(result);
+  });
+
   // 获取用户信息
-  app.get('/api/user/:id', (req, res) => {})
+  app.get('/api/user/:id', (req, res) => {
+    res.send('This is user info!');
+  });
   // 登录
+
 
 }
