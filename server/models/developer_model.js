b10c7be90c8ad9ca75530605517ae37cd6168b24
临渊 <flowflycat@users.noreply.github.com>
2016-11-21 11:24:07 +0800

    删去_id字段

--- server/models/developer_model.js
+++ server/models/developer_model.js
@@ -1,7 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 
 export default mongoose.model('developer', new Schema({
-  _id: Schema.Types.ObjectId,
   uid: String, // 用户id
   pid: String, // 参与项目id
   dev_skills: Array,   // 开发技术栈
