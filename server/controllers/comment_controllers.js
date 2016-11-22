bbac441547628eda327a9046932fe779d25e5856
729828947@qq.com <729828947@qq.com>
2016-11-22 18:48:42 +0800

    增加评论添加和查询的接口，创建utils目录 添加包装接口返回数据的 respond方法

--- server/controllers/comment_controllers.js
+++ server/controllers/comment_controllers.js
@@ -1,8 +1,25 @@
-import commentHandlers from '../handlers/comment_handlers.js';
-
+import CommentHandler from '../handlers/comment_handlers.js';
+const commentHandler = new CommentHandler();
 export default function (app) {
-  // 留言获取
-  // app.get('/api/comment/:id', (req, res) =>{});
-  // 留言
-  // app.post('/api/comment', (req, res) =>{});
+    // 留言获取
+    app.get('/api/comment/:id', (req, res) => {
+
+        commentHandler.fetchAllComment(1, 100).then(result=> {
+            res.send(result);
+        });
+    });
+
+    // 留言
+    app.post('/api/comment', (req, res) => {
+        console.log("comment");
+        let commentJson = {
+            user: {_id: '5832705b29911605406a201b', avator: 'lsjflsadjflkjak', nickname: 'tom'},
+            pid: 'ididi',
+            content: 'lf雷锋精神来看看肌肤'
+        };
+        commentHandler.addComment(commentJson)
+            .then(result=> {
+                res.send(result);
+            });
+    });
 }
--- server/handlers/comment_handlers.js
+++ server/handlers/comment_handlers.js
@@ -1,5 +1,28 @@
-import commentModel from '../models/comment_model';
+import CommentModel from '../models/comment_model';
+import {respond} from '../utils'
+export default class CommentHandler {
+    constructor() {
+    }
 
-export default function () {
-  
+    fetchAllComment(page, pageSize) {
+        return CommentModel.fetchAll(page, pageSize)
+            .then(comments=> {
+                return respond(0, '查询成功', comments, 1, pageSize);
+            }).catch(err=> {
+                return respond(110, '查询失败');
+
+            });
+    };
+
+
+    addComment(commentJson) {
+        return CommentModel.add(new CommentModel(commentJson))
+            .then(comment=> {
+                return respond(0, '评论成功', comment);
+            }).catch(err=> {
+                return respond(119, '评论失败');
+            })
+
+    }
+
 }
