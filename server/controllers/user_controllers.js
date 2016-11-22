dd760833040ba0107fcd80f4ad897292157961e5
suhanyujie <suhanyujie@qq.com>
2016-11-22 21:32:53 +0800

    添加用户注册的json测试数据

--- server/controllers/user_controllers.js
+++ server/controllers/user_controllers.js
@@ -3,14 +3,28 @@ import userHandlers from '../handlers/user_handlers.js';
 export default function (app) {
   // 用户注册
   app.get('/api/user/register', (req, res) => {
-
-
-
-    var result =  {
-      flag: 1,
-      msg: 'success~'
+    let userInfoJson = {
+      _id: '',
+      username: 'user1',
+      nickname: 'yunDuanXinYue',
+      passwd: '123456',
+      level: '1',
+      telephone:  '15079051079',
+      email: 'suhanyujie@qq.com',
+      skill: ['php','nodejs'],
+      access_token: 'xxxxx',
+      expire_time: '1457625320',
+      sns_type: 3,
+      avatar: 'http://fd.img.com/img1.png',
+      create_time: '2016-11-22 12:23:00',
+      last_login_time: '2016-11-22 15:23:00'
     };
-    res.send(result);
+
+    userHandlers.addUser(userInfoJson)
+        .then((result)=>{
+          res.send(result);
+        });
+
   });
 
   // 获取用户信息
