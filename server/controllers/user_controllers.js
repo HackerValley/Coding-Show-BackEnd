df4d7d67aae851a7039306ec44748f9120addcee
suhanyujie <suhanyujie@qq.com>
2016-11-23 21:10:02 +0800

    获取req中的表单数据

--- server/controllers/user_controllers.js
+++ server/controllers/user_controllers.js
@@ -3,23 +3,29 @@ const userHandle = new userHandlers();
 
 export default function (app) {
     // 用户注册
-    app.get('/api/user/register', (req, res) => {
-        let userInfoJson = {
+    app.post('/api/user/register', (req, res) => {
+        let defaultJson = {
             username: 'user1',
             nickname: 'yunDuanXinYue',
             passwd: '123456',
             level: '1',
             telephone:  '15079051079',
             email: 'suhanyujie@qq.com',
-            skill: ['php','nodejs'],
+            skill: [],
             access_token: 'xxxxx',
             expire_time: '1457625320',
-            sns_type: 3,
+            sns_type: 0,
             avatar: 'http://fd.img.com/img1.png',
             create_time: '2016-11-22 12:23:00',
             last_login_time: '2016-11-22 15:23:00'
         };
-
+        let userInfoJson = {
+            username: req.body.username,
+            email: req.body.email,
+            passwd: req.body.password,
+            create_time: new Date().toLocaleString()
+        };
+        userInfoJson = Object.assign(defaultJson, userInfoJson);
         userHandle.addUser(userInfoJson)
             .then((result)=>{
                 res.send(result);
