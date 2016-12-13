eed59d96635e842b72715d452b76611825c964c8
flowflycat <1058179212@qq.com>
2016-12-13 19:09:25 +0800

    project_model,developer_model 更新

--- server/models/developer_model.js
+++ server/models/developer_model.js
@@ -3,6 +3,6 @@ import mongoose, { Schema } from 'mongoose';
 export default mongoose.model('developer', new Schema({
   uid: String, // 用户id
   pid: String, // 参与项目id
-  dev_skills: Array,   // 开发技术栈
-  application_time: Date //申请时间
+  dev_skills: Array, // 开发技术栈
+  application_time: { type: Date, default: Date.now } // 申请时间
 }));
--- server/models/project_model.js
+++ server/models/project_model.js
@@ -7,11 +7,11 @@ export default mongoose.model('project', new Schema({
   detail: String, // 简介
   imagePath: Array, // 项目图片 字符串数组
   project_link: String, // 链接
-  developers: Array, // 开发者
-  develper_count: Number, // 开发者人数
+  developers: { type: Array, default: [] }, // 开发者
+  develper_count: { type: Number, default: 0 }, // 开发者人数
   star_count: { type: Number, default: 0 }, // 点赞数
-  star_users: Array, // 点赞用户 存储用户id
-  status: Number, // 可用/禁用
-  create_time: Date, // 创建时间
-  mod_time: Date // 最后修改时间
+  star_users: { type: Array, default: [] }, // 点赞用户 存储用户id
+  status: { type: Number, default: 0 }, // 可用/禁用
+  create_time: {type:Date, default : Date.now}, // 创建时间
+  mod_time: {type:Date, default : Date.now} // 最后修改时间
 }));
