23ef17fb58ab5da6e947e9ae3c88f2f912103ba8
yunnysunny <yunnysunny@gmail.com>
2017-03-07 22:35:12 +0800

    增加我开发的项目接口测试用例

--- server/test/mocha/project/project_mine_test.js
+++ server/test/mocha/project/project_mine_test.js
@@ -7,10 +7,25 @@ import {expect} from 'chai';
 import app from '../../../app';
 
 describe('project list', function() {
-    it('should get mine project list',function(done) {
+    it('should get release mine project list',function(done) {
         request(app)
             .get('/api/projects/mine')
             .query({query_type:'release'})
+            .set('Cookie','coding_show='+cookie)
+            .expect(200)
+            .end(function(err,res) {
+                if (err) {
+                    return done(err);
+                }
+                expect(res.body).to.have.property('data');
+                console.info('我发布的项目',res.body.data);
+                done();
+            });
+    });
+    it('should get mine project list',function(done) {
+        request(app)
+            .get('/api/projects/mine')
+            .query({query_type:'develop'})
             .set('Cookie','coding_show='+cookie)
             .expect(200)
             .end(function(err,res) {
